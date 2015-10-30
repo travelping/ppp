088147b1980f4b70f08388936ef1d0a024432b2e
Andreas Schultz <aschultz@tpip.net>
2015-10-30 15:31:48 +0100

    add LICENSE

--- include/ppp_ipcp.hrl
+++ include/ppp_ipcp.hrl
@@ -1,3 +1,7 @@
+%% This Source Code Form is subject to the terms of the Mozilla Public
+%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%% file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 -record(ipcp_opts, {
     neg_addr = false		:: boolean(),			%% Negotiate IP Address?
     req_addr = false		:: boolean(),			%% Ask peer to send IP address?
